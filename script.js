85ae2e3a86a190ffd02b3dad0f1bcada6b5c8644
Anil Rai <anilrai4001@gmail.com>
2023-07-18 20:29:15 +0530

    introduced funtions

--- script.js
+++ script.js
@@ -1,45 +1,100 @@
-const food = require('./food.json')
-console.log('');
+const food = require("./food.json");
 
-//1
-console.log(food);
-console.log('');
-
-//2-7
-let categoryArray = ['Vegetable','Fruit','Protein','Nuts','Grain','Dairy'];
-for(let i=0;i<categoryArray.length;i++){
-    let categoryObjects = food.filter((element)=>{
-        if(element.category == categoryArray[i])
-            return true;
-        return false;
-    });
-    console.log(categoryObjects);
-    console.log('');
+function one(food) {
+  console.log(food);
 }
 
-//8
-let calorieAbove100 = food.filter((element)=>{
-    if(element.calorie>100)
-        return true;
+function two(food) {
+  let categoryObjects = food.filter((element) => {
+    if (element.category == "Vegetable") return true;
     return false;
-})
-console.log(calorieAbove100);
-console.log('');
-
-//9
-let calorieBelow100 = food.filter((element)=>{
-    if(element.calorie<100)
-        return true;
+  });
+  console.log(categoryObjects);
+  console.log("");
+}
+function three(food) {
+  let categoryObjects = food.filter((element) => {
+    if (element.category == "Fruit") return true;
     return false;
-})
-console.log(calorieBelow100);
-console.log('');
+  });
+  console.log(categoryObjects);
+  console.log("");
+}
+function four(food) {
+  let categoryObjects = food.filter((element) => {
+    if (element.category == "Protein") return true;
+    return false;
+  });
+  console.log(categoryObjects);
+  console.log("");
+}
+function five(food) {
+  let categoryObjects = food.filter((element) => {
+    if (element.category == "Nuts") return true;
+    return false;
+  });
+  console.log(categoryObjects);
+  console.log("");
+}
+function six(food) {
+  let categoryObjects = food.filter((element) => {
+    if (element.category == "Grain") return true;
+    return false;
+  });
+  console.log(categoryObjects);
+  console.log("");
+}
+function seven(food) {
+  let categoryObjects = food.filter((element) => {
+    if (element.category == "Dairy") return true;
+    return false;
+  });
+  console.log(categoryObjects);
+  console.log("");
+}
 
-//10
-let decreasingArray = food.sort((a,b)=>{return b.protiens-a.protiens});
-console.log(decreasingArray)
-console.log('');
-//11
-let increasingArray = food.sort((a,b)=>{return a.cab-b.cab});
-console.log(increasingArray)
-console.log('');
+function eight(food) {
+  let calorieAbove100 = food.filter((element) => {
+    if (element.calorie > 100) return true;
+    return false;
+  });
+  console.log(calorieAbove100);
+  console.log("");
+}
+
+function nine(food) {
+  let calorieBelow100 = food.filter((element) => {
+    if (element.calorie < 100) return true;
+    return false;
+  });
+  console.log(calorieBelow100);
+  console.log("");
+}
+
+function ten(food) {
+  let decreasingArray = food.sort((a, b) => {
+    return b.protiens - a.protiens;
+  });
+  console.log(decreasingArray);
+  console.log("");
+}
+
+function eleven(food) {
+  let increasingArray = food.sort((a, b) => {
+    return a.cab - b.cab;
+  });
+  console.log(increasingArray);
+  console.log("");
+}
+
+one(food);
+two(food);
+three(food);
+four(food);
+five(food);
+six(food);
+seven(food);
+eight(food);
+nine(food);
+ten(food);
+eleven(food);
